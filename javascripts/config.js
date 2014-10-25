919a2cbe3357e8bbfbab601ecc591bceee0c7904
Sneezry <lizhe@lizhe.org>
2014-10-25 19:33:04 +0800

    blog

---
+++ javascripts/config.js
@@ -0,0 +1,18 @@
+var disqus_shortname = 'liruipete';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://lirui.org';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'liruipete';						//Your username of GitHub.
+var repos = 'liruipete.github.io';					//Name of repository that contains your blog.
+var sitetitle = 'Jahdo';						//Name of your blog.
+var rss = '';									//Rss address of your blog.
+var suffix = '';								//Suffix of post files, '.' should be included.
+var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
+
+//Sample Config:
+//var disqus_shortname = 'sneezry';
+//var hostbase = 'http://sneezry.com/blog';
+//var githubname = 'sneezry';
+//var repos = 'blog';	
+//var sitetitle = 'Sneezry';
+//var rss = '';
+//var suffix = '.md';
+//var branch = 'gh-pages';
